cc68c2828622c16b4a545c47d924eeecf7bdac41
Patricia-Berges <patry37.zgz@gmail.com>
2024-10-09 19:31:52 +0000

    Finally started this

--- src/js/views/home.js
+++ src/js/views/home.js
@@ -1,15 +1,45 @@
-import React from "react";
-import rigoImage from "../../img/rigo-baby.jpg";
+import React, {useEffect, useState} from "react";
 import "../../styles/home.css";
 
-export const Home = () => (
-	<div className="text-center mt-5">
-		<h1>Hello Rigo!</h1>
-		<p>
-			<img src={rigoImage} />
-		</p>
-		<a href="#" className="btn btn-success">
-			If you see this green button, bootstrap is working
-		</a>
-	</div>
-);
+export const Home = () => {
+
+	const mapData = (data) => {
+		return data.map((item) => {
+			return (
+				<div key={item.uid}>
+					<p>{item.name}</p>
+				</div>
+			)
+		})
+	}
+
+	const [people, setPeople] = useState([])
+	const [planets, setPlanets] = useState([])
+	const [vehicles, setVehicles] = useState([])
+
+	useEffect(() => {
+		fetch("https://www.swapi.tech/api/people")
+		.then(r => r.json())
+		.then(data => setPeople(data.results))
+	}, [])
+
+	useEffect(() => {
+		fetch("https://www.swapi.tech/api/planets")
+		.then(r => r.json())
+		.then(data => setPlanets(data.results))
+	}, [])
+
+	useEffect(() => {
+		fetch("https://www.swapi.tech/api/vehicles")
+		.then(r => r.json())
+		.then(data => setVehicles(data.results))
+	}, [])
+
+	return (
+		<div className="container d-flex">
+			<div className="m-3">{mapData(people)}</div>
+			<div className="m-3">{mapData(planets)}</div>
+			<div className="m-3">{mapData(vehicles)}</div>
+		</div>
+	)
+}
